03b0e6df03fd1d9f5f0a7caa29cd2dee9ed262c4
Jacek.Woj <jacek.wojtuszek@gmail.com>
2019-05-12 14:01:39 +0100

    koniec_zadania_minmax

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,17 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        int wynik = a[0];
+        for (int i = 1; i <= a.length; i++) {
+            if (a[i] < wynik) {
+                wynik = a[i];
+            }
+        }
+
+        System.out.println(wynik);
     }
 }
+
+
